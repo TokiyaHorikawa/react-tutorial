899e34118c754a34c7d92c80516fec3a727b00ab
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-24 17:07:12 +0900

    feat: make footer components

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from './Footer';
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Footer />
       </div>
     );
   }
