93e816699cb731395ee54eb1d4dc13f70e2133ed
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-24 17:41:31 +0900

    feat: test nest components for header title

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Title from "./Header/Title";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <header>header</header>
+      <header>
+        <Title />
+      </header>
     );
   }
 }
