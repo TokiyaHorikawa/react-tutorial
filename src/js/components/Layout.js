8dbb2ef260acbbca0b1b11f1c827321291424b3d
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-24 20:49:53 +0900

    test dom change

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,9 +3,17 @@ import Header from "./Header";
 import Footer from './Footer';
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "Tokiya"}
+  }
   render() {
+    setTimeout(() => {
+      this.setState({ name: "Horikawa"});
+    }, 10000);
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
