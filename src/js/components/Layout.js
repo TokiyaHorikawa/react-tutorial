37dc8982b8404c1b8d49555e58d31c4dcad6386e
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-24 17:05:34 +0900

    feat: make header components

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "Tsutomu";
-  }
   render() {
     return (
-      <h1>It's {this.name}!</h1>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
